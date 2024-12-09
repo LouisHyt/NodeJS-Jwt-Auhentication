a9b799d180ce8acb0a0a71d2ac888c79a5c8fc55
Louis | Diox <45403791+LouisHyt@users.noreply.github.com>
2024-12-09 23:15:06 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express"
+import 'dotenv/config'
+
+const { PORT } = process.env;
+const app = express()
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {
+        text: "Salut data"
+    })
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})

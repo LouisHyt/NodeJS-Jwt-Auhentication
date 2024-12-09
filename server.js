336cd97a7c8d6896266f7bd0bc137f5092dc32fe
Louis | Diox <45403791+LouisHyt@users.noreply.github.com>
2024-12-09 23:21:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 
 app.get("/", (req, res) => {

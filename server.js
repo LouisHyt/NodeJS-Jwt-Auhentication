90b8852a1556d43078d90a03d9d422b91cf35675
Louis | Diox <45403791+LouisHyt@users.noreply.github.com>
2024-12-09 23:27:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const { PORT } = process.env;
 const app = express()
 
 app.set('view engine', 'ejs');
-app.set('views', './views');
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
